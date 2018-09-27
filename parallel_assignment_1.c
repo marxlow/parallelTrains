69cd48fd15650f110a7a15978e3df599c7abf103
Low Jun Chen Marx <marxlow94@gmail.com>
2018-09-27 12:27:17 +0800

    Fix typo 7

--- parallel_assignment_1.c
+++ parallel_assignment_1.c
@@ -295,7 +295,7 @@ int main(int argc, char *argv[])
                 }
 #pragma omp critical
                 {
-                    // printf("\n~~~ Debug here ~~~ Train %d, with status: %d , with loading time %d, with station status: %d\n", i, green_trains[i].status, green_trains[i].loading_time, green_trains[i].station);
+                    printf("~~~ Debug here ~~~ Train %d, with status: %d , with loading time %d, with station status: %d\n", i, green_trains[i].status, green_trains[i].loading_time, green_stations[green_trains[i].station]);
                     // Train is waiting on an empty station, we can start loading
                     if (green_trains[i].status == IN_STATION && green_trains[i].loading_time == WAITING_TO_LOAD && green_stations[green_trains[i].station] == READY_TO_LOAD)
                     {
